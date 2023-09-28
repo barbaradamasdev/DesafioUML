314cb4e435dfc81c9fdfae7ea3a22d63c218d99c
Bárbara Damasceno <115722516+barbaradamasdev@users.noreply.github.com>
2023-09-28 17:56:19 -0300

    Add files via upload

---
+++ src/funcionalidades/navegadorDeInternet/NavegadorDeInternet.java
@@ -0,0 +1,8 @@
+package funcionalidades.navegadorDeInternet;
+public interface NavegadorDeInternet {
+    public void exibirPagina(); 
+
+    public void adicionarNovaAba();
+
+    public void atualizarPagina();
+}
